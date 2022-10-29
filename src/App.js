2438340f60697b6f1a9cecb39b29ad6e60d4ab6b
Malach Salama <legitdiamonds050@gmail.com>
2022-10-29 13:50:29 +0300

    Added links section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import profile from "./profile__img.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={profile} id="profile__img" alt="avatar" />
+        <p id="twitter">Malach Salama</p>
+        <p id="slack" className="Hidden">
+          Salama
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className="Links">
+        <a href="https://training.zuri.team/" id="btn__zuri">
+          Twitter Link
+        </a>
+        <a href="http://books.zuri.team" id="books">
+          Zuri Team
+        </a>
+        <a
+          href="http://books.zuri.team"
+          id="books"
+          title="This is a link to where you find books about design and coding"
+        >
+          Zuri Books
+        </a>
+        <a href="https://training.zuri.team/" id="btn__zuri">
+          Python Books
+        </a>
+        <a href="https://training.zuri.team/" id="btn__zuri">
+          Background Check for Coders
+        </a>
+        <a href="https://training.zuri.team/" id="btn__zuri">
+          Design Books
+        </a>
+      </div>
     </div>
   );
 }
